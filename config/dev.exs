a9a114d0b2996aa4a0540ec18d86345ea87c2f93
Anthony Gregis <anthony@gregis.dev>
2026-02-18 18:41:19 -0600

    fix(config): resolve issue with dev config failing build

--- config/dev.exs
+++ config/dev.exs
@@ -58,12 +58,12 @@ config :roomie, RoomieWeb.Endpoint,
     web_console_logger: true,
     patterns: [
       # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
       # Gettext translations
-      ~r"priv/gettext/.*\.po$"E,
+      ~r"priv/gettext/.*\.po$",
       # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/roomie_web/router\.ex$"E,
-      ~r"lib/roomie_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"lib/roomie_web/router\.ex$",
+      ~r"lib/roomie_web/(controllers|live|components)/.*\.(ex|heex)$"
     ]
   ]
 
